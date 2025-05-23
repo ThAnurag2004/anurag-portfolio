dca143b57cf628e8ca8ad8711dfdca9ecd35ee8f
Anurag Singh <abc103498@gmail.com>
2025-05-23 13:36:02 +0530

    stage7

--- src/constants/Index.js
+++ src/constants/Index.js
@@ -1,6 +1,7 @@
 import project1 from "../assets/projects/project-1.webp";
 import project3 from "../assets/projects/project-3.webp";
 import project2 from "../assets/projects/project-2.webp";
+import project4 from "../assets/projects/project-4.webp"
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1 years of hands-on experience, I have honed my skills in front-end technologies like React, GSAP and FramerMotion, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -31,6 +32,14 @@ export const PROJECTS = [
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
     technologies: ["Tailwind CSS", "React", "OpenWeather"],
+  },
+  {
+    title: "KaamKhoj Job Portal",
+    link: "",
+    image: project4,
+    description:
+      "A Job searching and job posting platform with role based login for admin and user",
+    technologies: ["Tailwind CSS", "React", "Shadcn Ui","Clerk Js", "Supabase"],
   },
 ];
 
